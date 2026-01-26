1b69ecd4eefd11594ca7bd03a6629db5ce218b1d
Vladislav Kochenkov <priznavologia@gmail.com>
2026-01-26 07:49:11 +0300

    Добавил AssetProvider для загрузки объектов в RunTime и использования Addressables

---
+++ Assets/Project/Code/Infrastructure/AssetManagement/IAsset.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using UnityEngine.AddressableAssets;
+using UnityEngine;
+using Code.Infrastructure.Services;
+
+namespace Code.Infrastructure.AssetManagement
+{
+    public interface IAsset : IService
+    {
+        Task<GameObject> Instantiate(string path);
+        Task<GameObject> Instantiate(string path, Vector3 at);
+        Task<GameObject> Instantiate(string path, Transform parent);
+        Task<T> Load<T>(AssetReference assetbReference) where T : class;
+        Task<T> Load<T>(string address) where T : class;
+        void CleanUp();
+        void Initialize();
+    }
+}
