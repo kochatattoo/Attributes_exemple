d112983ac019a2f26f004578ec599d7fd979d005
Vladislav Kochenkov <priznavologia@gmail.com>
2026-01-30 16:47:53 +0300

    Realise

---
+++ Assets/Project/Code/Hero/Items/ConsumableItem.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Code.Hero.Items
+{
+    [CreateAssetMenu(fileName = "NewConsumable", menuName = "Items/Consumable")]
+    public class ConsumableItem : ItemConfig
+    {
+        public float Duration; // Длительность эффекта в секундах
+    }
+}
